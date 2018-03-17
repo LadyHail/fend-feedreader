b6d510a9306d44b86dc394e4703598094e842db6
LadyHail <ladydeath@o2.pl>
2018-03-17 22:45:04 +0100

    Add initial entries test

--- src/jasmine/spec/feedreader.js
+++ src/jasmine/spec/feedreader.js
@@ -73,14 +73,15 @@ $(function() {
           */
         it('menu changes visibility when the menu icon is clicked', function() {
             var menuIcon = $('.menu-icon-link');
-            
+
             menuIcon.click();
             expect(body.hasClass('menu-hidden')).toBeFalsy();
             menuIcon.click();
             expect(body.hasClass('menu-hidden')).toBeTruthy();
         });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+        /* TODO: Write a new test suite named "Initial Entries" */
+        describe('Initial Entried', function() {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -88,8 +89,20 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+            beforeEach(function(done) {
+                loadFeed(0, function() {
+                    done();
+                });
+            });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+            it('there is at least a single .entry element within .feed container', function(done) {
+                var entry = $('.entry');
+                expect(entry.length).not.toBe(0);
+                done();
+            });
+        });
+
+        /* TODO: Write a new test suite named "New Feed Selection" */
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
